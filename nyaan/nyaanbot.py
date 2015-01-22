5a2a0f4b420772006b71e224d5601663266fd4c2
contee <contee213@gmail.com>
2015-01-22 21:02:26 +0900

    update

--- nyaan/nyaanbot.py
+++ nyaan/nyaanbot.py
@@ -10,14 +10,16 @@ this bot tweet nyaan.
 
 """
 
-import time
+import time, datetime
 import twitter
 import heapq
 import traceback
 import itertools
 import daemon
+import random
 import logging
 from nyaan.auth import oauth, oauth2
+from nyaan.log import setup_logging
 
 logger = logging.getLogger(__name__)
 
@@ -63,6 +65,26 @@ class TwitterBot():
         # self.search_client = twitter.Twitter(auth=oauth2())
         self.pub_stream = twitter.TwitterStream(auth=oauth(), domain='stream.twitter.com')
         self.scheduler = Scheduler(tasks=[ScheduledTask(self.retweet_nyaan, 5)])
+        self.last_nyaan = dict()
+        self.last_reset = datetime.datetime.now()
+        self.count_nyaan = 0
+        self.last_count_nyaan = 0
+
+    def _clear(self):
+        self.last_nyaan = dict()
+        self.last_reset = datetime.datetime.now()
+        self.count_nyaan = 0
+
+    def nyaan_stat(self):
+        delta = datetime.datetime.now() - self.last_reset
+        if delta.total_seconds() < 86400:
+            return
+        msg = ''
+        if self.last_count_nyaan > 0:
+            msg += '昨日は' + self.last_count_nyaan + 'にゃーんしたにゃん。\n'
+        msg += '今日は' + self.count_nyaan + 'にゃーんだにゃん。\n明日もがんばるにゃん。\n'
+        self.client.statuses.update(status=msg)
+        self._clear()
 
     def retweet_nyaan(self):
         track_str = self._create_nyaan_track()
@@ -70,24 +92,42 @@ class TwitterBot():
         logger.debug(track_str)
         logger.debug(response)
         for msg in response:
-            if not msg['in_reply_to_user_id'] and not msg['entities']['user_mentions']:
-                logger.info(msg['user']['screen_name'] + ':' + msg['text'])
-                self.client.statuses.retweet(id=msg['id'])
+            now = time.time()
+            if len(msg['text']) > 0:
+                continue
+            if msg['in_reply_to_user_id'] or msg['entities']['user_mentions']:
+                continue
+            if self.is_kimagure():
+                continue
+            if msg['user']['id'] in self.last_nyaan:
+                # ランダム時間の連続制限
+                if self.last_nyaan[msg['user']['id']] < now:
+                    continue
+            logger.info(msg['user']['screen_name'] + ':' + msg['text'])
+            self.client.statuses.retweet(id=msg['id'])
+            self.last_nyaan[msg['user']['id']] = now + random.randrange(0, 30, 1)
+            self.nyaan_stat()
 
     def _create_nyaan_track(self):
         track_str = []
-        base = ['にゃん','にゃーん','にゃ〜ん']
-        affix = ['', '！', '？', '♡', '♪', '☆']
-        for s in itertools.product(base, affix):
+        prefix = ['', 'にゃ', 'にゃん']
+        base = ['にゃん','にゃーん','にゃ〜ん', 'にゃおーん', 'にゃお〜ん', 'にゃああん']
+        affix = ['', '！', '？', '♡', '♥', '♪', '☆', '。', '.', '...', '・・・']
+        for s in itertools.product(prefix, base, affix):
             track_str.append(''.join(s))
-        return ','.join(track_str)
+        tsj = ','.join(track_str)
+        logger.info(tsj)
+        return tsj
 
+    def is_kimagure(self):
+        return False
 
     def run(self):
         logger.info("start")
         while True:
             logger.info("--- loop start ---")
             try:
+                self._clear()
                 self.scheduler.run_forever()
             except twitter.TwitterError:
                 logger.error(traceback.format_exc())
@@ -96,9 +136,11 @@ class TwitterBot():
             except:
                 logger.error(traceback.format_exc())
             logger.info("--- loop next ---")
+            time.sleep(30)
         logger.info("end")
 
 def main():
+    setup_logging()
     bot = TwitterBot()
     return bot.run()
 
